17e4ffa379c8dbf5505898a6786080f9ba65808d
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-03-11 17:42:11 +0530

    Adding Spanish language features

--- hello_test.go
+++ hello_test.go
@@ -4,18 +4,24 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying 'Hello, World' when an empty string is passed", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+	    got := Hello("Elodie", "Spanish")
+	    want := "Hola, Elodie"
+	    assertCorrectMessage(t, got, want)
+  })
 }
 
 func assertCorrectMessage(t *testing.T, got, want string) {
